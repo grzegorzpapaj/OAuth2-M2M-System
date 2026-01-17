15deb31f118c22d1a26094a15a29632661bafcc6
grzegorzpapaj <137798629+grzegorzpapaj@users.noreply.github.com>
2026-01-17 09:34:39 +0100

    Add /register endpoint

--- crypto-server/main.py
+++ crypto-server/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 
-from .database import Base, engine
+from .database import engine, Base, get_db
 from .models import ClientApp  # Import potrzebny, żeby Base o nich wiedział
 from .models import CurrencyRate
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ClientCreate(BaseModel):
+    client_id: str
+    client_secret: str
+    app_name: str
 
 
 # To zdarzenie wykona się PRZED startem serwera
@@ -15,6 +23,30 @@ async def startup():
         await conn.run_sync(Base.metadata.create_all)
     print("Database and tables created")
 
+
+@app.post("/register")
+async def register_client(client_data: ClientCreate, db: AsyncSession = Depends(get_db)):
+
+    # 1. Sprawdź, czy taki client_id już istnieje
+    result = await db.execute(select(ClientApp).where(ClientApp.client_id == client_data.client_id))
+    existing_client = result.scalars().first()
+    
+    if existing_client:
+        raise HTTPException(status_code=400, detail="Taki Client ID już istnieje")
+
+    # 2. Utwórz nowy obiekt (trzeba zrobic hashowanie)
+    new_client = ClientApp(
+        client_id=client_data.client_id,
+        client_secret=client_data.client_secret, # Tutaj powinno być hashowanie
+        app_name=client_data.app_name
+    )
+
+    # 3. Zapisz w bazie
+    db.add(new_client)
+    await db.commit()
+    await db.refresh(new_client)
+
+    return {"message": "Klient zarejestrowany", "id": new_client.id, "app_name": new_client.app_name}
 
 @app.get("/")
 async def root():
